ac2b4f8b41965da9e7047a885437fabcb70ebe1c
Amanda <daikiraidemodaisuki@gmail.com>
2018-08-30 07:25:24 +0000

    creator can now add amount

--- ticker/public/js/script.js
+++ ticker/public/js/script.js
@@ -1,2 +1,11 @@
 var d = new Date();
 document.querySelector(".year").innerHTML = d.getFullYear();
+
+function showAddAmountForm() {
+    var x = document.getElementById("addAmount");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- ticker/routes/items.js
+++ ticker/routes/items.js
@@ -113,6 +113,7 @@ router.put("/items/:id", function(req, res) {
                             if (user.items[i]._id.equals(req.params.id)) {
                                 // source: https://stackoverflow.com/questions/8976627/how-to-add-two-strings-as-if-they-were-numbers
                                 user.items[i].amount = +user.items[i].amount + +req.body.amount;
+                                user.items[i].total = +user.items[i].total - +req.body.amount;
                             }
                         }
                         user.save();
@@ -121,19 +122,52 @@ router.put("/items/:id", function(req, res) {
                 });
             }
             else {
-                req.flash("failure", "You don't have anymore " + item.name + " in stock!");
+                req.flash("failure", "You don't have enough " + item.name + " in stock!");
                 res.redirect("back");
             }
         }
     });
 });
 
-// router.put("/users", function(req, res) {
-//     User.findByIdAndUpdate(req.user._id, req.user, function(err, user) {
-//         user._id = req.user._id;
-//         user.username = req.user.username;
-        
-//     });
-// });
+router.put("/items/:id/add", function(req, res) {
+    Item.findById(req.params.id, function(err, item) {
+        if (err || !item) {
+            if(!item) {
+                req.flash("failure", "Item not found");
+            }
+            else {
+                req.flash("failure", "Something went wrong");
+            }
+            console.log("POST: /items/:id", err);
+            res.redirect("back");
+        }
+        else {
+            item.total = +item.total + +req.body.total;
+            item.save();
+            User.findById(req.user.id, function(err, user) {
+                if (err || !user) {
+                    if(!user) {
+                        req.flash("failure", "User not found");
+                    }
+                    else {
+                        req.flash("failure", "Something went wrong");
+                    }
+                    console.log("POST: /items/:id", err);
+                    res.redirect("back");
+                }
+                else {
+                    for (var i=0; i<user.items.length;i++) {
+                        if (user.items[i]._id.equals(req.params.id)) {
+                            // source: https://stackoverflow.com/questions/8976627/how-to-add-two-strings-as-if-they-were-numbers
+                            user.items[i].total = +user.items[i].total + +req.body.total;
+                        }
+                    }
+                    user.save();
+                    res.redirect("/items/" + req.params.id);
+                }
+            });
+        }
+    });
+});
 
 module.exports = router;
